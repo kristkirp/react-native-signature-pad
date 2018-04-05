96169f1e9084fe4c88cff6f07d68129caeb1138c
kristkirp <30670856+kristkirp@users.noreply.github.com>
2018-04-05 09:21:01 -0400

    fix issue with proptypes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import {
   View,
   WebView,
   StyleSheet,
+  ViewPropTypes
 } from 'react-native';
+import PropTypes from 'prop-types';
 
 
 import htmlContent from './injectedHtml';
@@ -19,7 +21,7 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
